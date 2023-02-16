52ec40cf47b1b4e5ad49f1932596f0cc8f806704
Maalaki <manuel.ogiez@gmail.com>
2023-02-16 22:15:48 +0100

    ajout de la base de donnée et peuplement avec des fixtures (faker)

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use Faker\Generator;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+
+    /**
+     * @var Generator
+     */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Création d'une boucle qui va créer 50 articles aléatoires
+        // Chaque article aura un titre, un contenu, une date de publication qui seront générés aléatoirement
+        for ($i=1; $i <= 50; $i++) {
+            $article = new User();
+            $article->setEmail($this->faker->email())
+                ->setPassword($this->faker->password());
+
+            $manager->persist($article);
+        }
+        $manager->flush();
+    }
+}
